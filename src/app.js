4099d68b9c59a6ddf625482fade9fb7be033020d
niu2x <niu2x@icloud.com>
2025-04-25 13:34:41 +0800

    fix

--- src/app.js
+++ src/app.js
@@ -1,18 +1,29 @@
 import React from "react"
+// import {createStateVariable} from "./helper.js"
 
 class App extends React.Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			currentToolIndex: 0,
+		}
+	}
 	render() {
 		return React.createElement(
 			"ul", {
-				className: "ingredients"
+				// className: JSON.stringify(this.props)
 			},
-			React.createElement('h1', null, 'hhhh'),
-			React.createElement('h1', null, 'hhhh'),
-			React.createElement('h1', null, 'hhhh')
+			<p>currentToolIndex: {this.state.currentToolIndex}</p>,
+			... this.props.toolsConfig.map((x, index)=>{
+				return <button onClick={()=>{
+					this.setState({currentToolIndex: index})
+				}}>{x.name} {index}</button>
+			})
 		);
 	}
 };
 
-export function createApp() {
-	return (<App></App>)
+export function createApp(toolsConfig) {
+	return <App toolsConfig={toolsConfig} />
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import {createApp} from "./app.js"
+import toolsConfig from "./tools-config.js"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(createApp());
+root.render(createApp(toolsConfig));
