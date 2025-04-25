c3317aa5053c1c54eb150e6e6b1151ba6e2e2f29
niu2x <niu2x@icloud.com>
2025-04-25 14:06:53 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import {createApp} from "./app.js"
 import toolsConfig from "./tools-config.js"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(createApp(toolsConfig));
--- src/tools-menu.js
+++ src/tools-menu.js
@@ -4,5 +4,30 @@ export default class ToolsMenu extends React.Component {
   constructor(props) {
     super(props)
   }
-  render() {}
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">GameEditor</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              {
+                this.props.toolsConfig.map((item, index)=>{
+                  return (
+                    <li className="nav-item" key={index}>
+                      <a className="nav-link" href="#">{item.name}</a>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+      </div>
+      </nav>
+    )
+  }
 };
